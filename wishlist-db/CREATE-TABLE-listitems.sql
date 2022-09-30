112dd90a8bf07d847ff9e57dcd05e7828e175f22
Tim Ross <personal.timross@gmail.com>
2022-09-29 23:59:05 -0700

    wishlists folder

---
+++ wishlist-db/CREATE-TABLE-listitems.sql
@@ -0,0 +1,7 @@
+CREATE TABLE listitems(
+    liID SERIAL PRIMARY KEY,
+    lID INT REFERENCES lists NOT NULL,
+    iID INT REFERENCES items NOT NULL,
+    liDesired INT DEFAULT 1,
+    liPurchased INT NOT NULL DEFAULT 0
+);
