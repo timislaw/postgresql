154b7c78115e733d5710a9aa790f69902b21985d
Tim Ross <personal.timross@gmail.com>
2022-10-01 14:39:31 -0700

    wk6-1

---
+++ wishlist-db/ALTER-TABLE-people.sql
@@ -0,0 +1,5 @@
+ALTER TABLE people
+    ADD COLUMN plastname VARCHAR(50) NOT NULL,
+    ADD COLUMN pcity VARCHAR(50) NOT NULL,
+    ADD COLUMN pstate VARCHAR(2) NOT NULL;
+    
---
+++ wk6/wk6-1.sql
@@ -0,0 +1,3 @@
+-- SELECT DISTINCT pcity FROM people ORDER BY pcity
+SELECT DISTINCT pcity FROM people ORDER BY pcity ASC;
+-- NOTE: I read on documentation that ASC is default ordering
